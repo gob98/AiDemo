25b81d59c048572c9fa65cf450ad0aa9873ee3f8
Gavin <C22746331@mytudublin.ie>
2023-04-01 10:07:41 +0100

    Added click to code

--- Assets/Scripts/AiController.cs
+++ Assets/Scripts/AiController.cs
@@ -15,6 +15,16 @@ public class AiController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,out RaycastHit hit))
+            SetAiTargetLocation(hit.point);
+        }
+    }
+
+    private void SetAiTargetLocation(Vector3 targetLocation)
+    {
+        _navMeshAgent.SetDestination(targetLocation);
     }
 }
